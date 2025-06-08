f27677a84564fc6c0e57de9037b2815c85b0613c
JackyLiuAI <jqliu2004@outlook.com>
2025-06-08 16:19:10 +0800

    修改sh脚本

---
+++ scripts/absa.sh
@@ -0,0 +1,6 @@
+python src/train_ABSA.py \
+--batch 16 \
+--epochs 10 \
+--lr 1e-5 \
+--adapter \
+# --lr_schedule \
---
+++ scripts/abte.sh
@@ -0,0 +1,6 @@
+python src/train_ABTE.py \
+--batch 16 \
+--epochs 10 \
+--lr 1e-5 \
+--adapter \
+# --lr_schedule \
